8c6236b394aa9e24ff61055605ba87fbd4e03bf9
tom tank engine <7099@trinity.croydon.sch.uk>
2025-11-21 13:07:13 +0000

    Add files via upload

---
+++ Concatenation 21-11-25.py
@@ -0,0 +1,9 @@
+name = input("What's you name?")
+age = int(input("What's your age?"))
+height = float(input("How tall are you in meters?"))
+
+print('My name is Tom, nice to meet you')
+print("You're called", name, "and you are", age,"years old")
+print("In 5 years time you will be", age + 5)
+print("If you're", height,"in meters, then you are", height * 100,"in centimeters")
+
